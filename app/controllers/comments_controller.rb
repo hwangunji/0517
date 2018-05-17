ef92f3a0a55f67fb3d8927e975ec7bbb450b047e
hwangunji <happyun5@naver.com>
2018-05-17 11:38:57 +0000

    0517

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+    def create
+        params.permit!
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.new(params[:comment])
+        
+        @comment.save
+        redirect_back(fallback_location: :back) 
+    end
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_back(fallback_location: :back) 
+    end
+end
